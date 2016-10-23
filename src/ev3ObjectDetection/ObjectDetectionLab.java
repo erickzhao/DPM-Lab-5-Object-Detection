cc36aabcc4bb433b36d3c95de049e4e687f8a144
erick <erick@hotmail.ca>
2016-10-23 16:17:22 -0400

    Add LCD menu

--- src/ev3ObjectDetection/ObjectDetectionLab.java
+++ src/ev3ObjectDetection/ObjectDetectionLab.java
@@ -22,7 +22,6 @@ public class ObjectDetectionLab {
 	public static void main(String[] args) {
 		
 		int buttonChoice;
-		
 		final TextLCD t = LocalEV3.get().getTextLCD();
 		
 		do {
@@ -30,25 +29,32 @@ public class ObjectDetectionLab {
 			
 			t.drawString("< Left | Right >", 0, 0);
 			t.drawString("       |        ", 0, 1);
-			t.drawString(" do    | nav    ", 0, 2);
-			t.drawString(" simple| around ", 0, 3);
-			t.drawString(" nav   | object ", 0, 4);
+			t.drawString(" detect| push   ", 0, 2);
+			t.drawString(" object| block  ", 0, 3);
 			
 			buttonChoice = Button.waitForAnyPress();
 		} while (buttonChoice != Button.ID_LEFT && buttonChoice != Button.ID_RIGHT);
 		
-		//set up color sensor
-		@SuppressWarnings("resource")
-		SensorModes colorSensor = new EV3ColorSensor(colorPort);
-		SampleProvider colorValue = colorSensor.getMode("RGB");
-		float[] colorData = new float[colorValue.sampleSize()];
 		
-		//initialize color reader and LCD display
-		ColorReader colorReader = new ColorReader(colorValue,colorData);
-		LCDInfo lcd = new LCDInfo(colorReader);
-		colorReader.start();
+		if (buttonChoice == Button.ID_LEFT) {
+			//set up color sensor
+			@SuppressWarnings("resource")
+			SensorModes colorSensor = new EV3ColorSensor(colorPort);
+			SampleProvider colorValue = colorSensor.getMode("RGB");
+			float[] colorData = new float[colorValue.sampleSize()];
+			
+			//initialize color reader and LCD display
+			ColorReader colorReader = new ColorReader(colorValue,colorData);
+			LCDInfo lcd = new LCDInfo(colorReader);
+			colorReader.start();
+			
+			while(Button.waitForAnyPress() != Button.ID_ESCAPE);
+			System.exit(0);
+		} else {
+			
+		}
 		
-		while(Button.waitForAnyPress() != Button.ID_ESCAPE);
-		System.exit(0);
+		
+		
 	}
 }
